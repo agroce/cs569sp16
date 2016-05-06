4869a58e855bfc035fd04b9d8fca51087faf92ff
Shawn <Lee@10-248-175-58.wireless.oregonstate.edu>
2016-05-05 21:56:38 -0700

    Update

--- projects/lix6/Part2/tester1.py
+++ projects/lix6/Part2/tester1.py
@@ -2,6 +2,7 @@ import sut
 import random
 import sys
 import time
+import math
 
 
 TIMEOUT  = int(sys.argv[1])
@@ -12,73 +13,127 @@ FAULTS   = int(sys.argv[5])
 COVERAGE = int(sys.argv[6])
 RUNNING  = int(sys.argv[7])
 
-
-
-rgen = random.Random()
-
-
-LAYER_BUDGET = TIMEOUT/DEPTH
-
-print "LAYER BUDGET:",LAYER_BUDGET
-
-slack = 0.0
-
-sut = sut.sut()
+#global variables are defined here
+sut=sut.sut()
 sut.silenceCoverage()
-
-sut.restart()
-
-queue = [sut.state()]
-visited = []
-
-startAll = time.time()
+rgen = random.Random()
+rgen.seed(SEED)
 
 actCount = 0
-elapsed = 0
-d = 1
-while d <= DEPTH:
-    print "DEPTH",d,"QUEUE SIZE",len(queue),"VISITED SET",len(visited)
-    d += 1
-    frontier = []
-    startLayer = time.time()
-    scount = 0
-    for s in queue:
-        scount += 1
-        sut.backtrack(s)
-        allEnabled = sut.enabled()
-        rgen.shuffle(allEnabled)
-        for a in allEnabled:
-            elapsed = time.time() - startLayer
-            if elapsed >= LAYER_BUDGET:
-                break
-            ok = sut.safely(a)
-            actCount += 1
-            if not ok:
-                print "FOUND A FAILURE"
-                #sut.prettyPrintTest(sut.test())
-                print sut.failure()
-                print "REDUCING"
-                R = sut.reduce(sut.test(),sut.fails, True, True)
-                sut.prettyPrintTest(R)
-                print sut.failure()
-                #sys.exit(1)
-            s2 = sut.state()
-            if s2 not in visited:
-                visited.append(s2)
-                frontier.append(s2)
-            sut.backtrack(s)
-        if elapsed >= LAYER_BUDGET:
-            print "DID NOT GET TO EXPAND",len(queue)-scount,"STATES"
-            break
-    elapsed = time.time() - startLayer
-    slack = float(LAYER_BUDGET-elapsed)
-    print "SLACK",slack
-    if (d < DEPTH) and (slack > 0):
-        LAYER_BUDGET = LAYER_BUDGET+slack/(DEPTH-d)
-        print "NEW LAYER BUDGET",LAYER_BUDGET
-    queue = frontier
+
+bugs = 0
+coverage = {}
+bugQueue = []
+newQueue = []
+
+
+
+
+def randomAction():
+    global actCount, bugs, bugQueue
+    act = sut.randomEnabled(rgen)
+    actCount += 1
+    ok = sut.safely(act)
+    if not ok:
+        bugs += 1
+        print "FOUND A FAILURE"
+        print sut.failure()
+        print "REDUCING"
+        bugQueue.append(sut.test())
+        collectCoverage()
+        R = sut.reduce(sut.test(),sut.fails, True, True)
+        sut.prettyPrintTest(R)
+        print sut.failure()
+        sut.restart()
+    else:
+    	#Find the new Statements
+        if len(sut.newStatements()) != 0:
+        	visited = list(sut.test())
+        	test = sut.reduce(visited, sut.coversStatements(sut.newStatements()))
+        	sut.replay(visited)
+        	newQueue.append((test,set(sut.currStatements())))
+    return ok  
+
+
+
+def buildQueue():
+	global activePool, belowMedian
+
+	belowMedian = set([])
+
+	visitedQueue = sorted(coverage.keys(), key = lambda x: coverage[x])
+
+	t = visitedQueue[len(coverage) / 2]
+	coverMedian = coverage[t]
+
+	for s in visitedQueue:
+		if coverage[s] < coverMedian:
+			belowMedian.add(s)
+		else:
+			break
+
+	newBelowMedian = set([])
+
+	coverSum = sum(map(lambda x:coverage[x],belowMedian))
+	coverMedian = coverSum / (1.0*len(belowMedian))
+	for s in belowMedian:
+		if coverage[s]<coverMedian:
+			newBelowMedian.add(s)
+	print len(newBelowMedian),"STATEMENTS BELOW MEDIAN BELOW MEDIAN COVERAGE OUT OF", len(belowMedian)
+	belowMedian = newBelowMedian
+	activePool = []
+	for (t,c) in newQueue:
+		for s in c:
+			if s in belowMedian:
+				activePool.append((t,c))
+				break
+
+print "STARTING PHASE 1"
+
+
+start = time.time()
+
+while(time.time() - start < TIMEOUT * 0.5):
+    sut.restart()
+    for s in xrange(0,DEPTH):
+    	if not randomAction():
+    		break
+    for s in sut.currStatements():
+   		if s not in coverage:
+   			coverage[s] = 0
+   		coverage[s] += 1
+
+
+
+print "STARTING PHASE 2"
+
+
+start2 = time.time()
+while time.time() - start2 < TIMEOUT * 0.5:
+	buildQueue()
+	cover = set([])
+	sut.restart()
+	# if rgen.random() > 0.7:
+	# 	sut.replay(rgen.choice(activePool)[0])
+	# 	cover = set(sut.currStatements())
+	for s in xrange(0, DEPTH):
+		if not randomAction():
+			break
+	for s in sut.currStatements():
+   		if s not in coverage:
+   			coverage[s] = 0
+   		coverage[s] += 1
+
+
 sut.internalReport()
 
-print "SLACK",slack
+print bugs, "FAILED"
 print "TOTAL ACTIONS",actCount
-print "TOTAL RUNTIME",time.time()-startAll
+print "TOTAL RUNTIME",time.time()-start
+
+
+
+
+
+        
+
