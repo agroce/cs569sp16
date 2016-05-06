bf299383f61087b47a8fdd27ac3dcce1f5e427bd
hafgham <hafed_g@hotmail.com>
2016-05-05 23:03:53 -0700

    Newer Version of My tester.py

--- projects/alghamha/Part 2/tester1.py
+++ projects/alghamha/Part 2/tester1.py
@@ -6,24 +6,31 @@ import os
 
 
 # Terminate the program with time
+# You can use 60 as a default Value
 TIMEOUT = int(sys.argv[1])
 
-# Determines the random seed for testing
+# Determines the random seed for testing. This should be assigned 0 when using the MEMORY/WIDTH
+# You can use 12 as a default Value
 SEEDS = int(sys.argv[2])
 
 # TEST_LENGTH or Depth
+# You can use 100 as a default Value
 DEPTH = int(sys.argv[3])
 
 # MEMORY or Width, the number of "good" tests to store
+# You can use a 100 as a default Value when testing combination lock faults
 MEMORY = int(sys.argv[4])
 
 # Enable/Disable Faults
+# You can use 1 as a default Value
 FaultsEnabled = int(sys.argv[5])
 
 # Enable/Disable Coverage
+# You can use 1 as a default Value
 CoverageEnabled = int(sys.argv[6])
 
 # Enable/Disable Running
+# You can use 1 as a default Value
 RunningEnabled = int(sys.argv[7])
 
 
@@ -32,14 +39,14 @@ if (SEEDS > 0 ):
 else:
 	rgen = random.Random(None)
 
+# gloable variables initilization
 sut = sut.sut()
 sut.silenceCoverage()
 bugs = 0
-#sut.restart()
 goodTests = []
 startTime = time.time()
-PopulationList = []
 
+# Function To Save The Faults
 def	saveFaults(elapsedFailure, fault, act, bug, REDUCING):
 	FileName = 'failure'+str(bug)+'.test'
 	file = open(FileName, 'w+')
@@ -47,6 +54,7 @@ def	saveFaults(elapsedFailure, fault, act, bug, REDUCING):
 	print >> file, fault, "\n"
 	print >> file, " Reduced Test Case \n"	
 	i = 0
+	# Reducing the Test Case
 	for s in REDUCING:
 		steps = "# STEP " + str(i)
 		print >> file, sut.prettyName(s[0]).ljust(80-len(steps),' '),steps
@@ -54,10 +62,7 @@ def	saveFaults(elapsedFailure, fault, act, bug, REDUCING):
 	file.close()
 	print fault
 
-startTime = time.time()
-
-
-
+# Sequntial algorithm that will traverse over all actions and execute them one by one
 for act in sut.enabled():
 	seq = sut.safely(act)
 	if (not seq) and (FaultEnabled == 1):
@@ -73,16 +78,18 @@ for act in sut.enabled():
 		saveFaults(elapsedFailure, Fault, act, bugs, REDUCING)
 		sut.restart()
 
+# RandomTester based on randomly selcted propability
 while (time.time() - startTime <= TIMEOUT):
-
+	# This will work only Memory input is set. It is good for finding combanition luck faults
 	if (len(goodTests) > 0) and (rgen.random() < 0.5):
 		sut.backtrack(rgen.choice(goodTests)[1])
 	else:
 		sut.restart()
+	# Based on the depth randonly execute an action
 	for s in xrange(0,DEPTH):
-		
 		action = sut.randomEnabled(rgen)
 		r = sut.safely(action)
+		# Start saving discovered fault on Disk
 		if (not r) and (FaultsEnabled == 1):
 			elapsedFailure = time.time() - startTime
 			bugs += 1
@@ -90,30 +97,30 @@ while (time.time() - startTime <= TIMEOUT):
 			sut.prettyPrintTest(sut.test())
 			test = sut.test()
 			Fault = sut.failure()
+			# Start Reducing the Test Case
 			print "REDUCING"
 			REDUCING = sut.reduce(sut.test(),sut.fails, True, True)
 			sut.prettyPrintTest(REDUCING)
+			#Saving discovered fault on Disk
 			saveFaults(elapsedFailure, Fault, act, bugs, REDUCING)
+			# Rest the system state
 			sut.restart()
-			#sut.prettyPrintTest(sut.test())
-			#print 
+		# Print the new discovered branches	
 		if (len(sut.newBranches()) > 0) and (RunningEnabled == 1):
 			print "ACTION:",action[0]
 			elapsed1 = time.time() - startTime
 			for b in sut.newBranches():
-			#print time.time(),len(sut.allBranches()),'NEW BRANCHES:', sut.newBranches()
 				print elapsed1,len(sut.allBranches()),"New branch",b
-		
+		# When getting new branches, save the test case into goodTest list to be executed based on random propability
 		if ((MEMORY != 0) and (len(sut.newBranches()) > 0)):
 			goodTests.append((sut.currBranches(), sut.state()))
 			goodTests = sorted(goodTests, reverse=True)[:MEMORY]
+		# Cleanup goodTest list based on the length of the goodTests
 		elif (MEMORY != 0) and (len(sut.newBranches()) == 0) and (len(goodTests) >= MEMORY):
 			RandomMemebersSelection = random.sample(goodTests,int(float((len(goodTests))*.20)))
 			for x in RandomMemebersSelection:
 				goodTests.remove(x)
-			
-	#print goodTests
-# This is not working...........
+# Printing Report
 elapsed = time.time() - startTime
 print "\n                  ############ The Final Report ############# \n"
 print elapsed, "Total Running Time"
