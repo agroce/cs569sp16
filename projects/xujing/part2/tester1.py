7bfcc2df0b91f7fca77a88f0231fdf86e0accb2b
JingyuanXu <skybluries@gmail.com>
2016-05-07 16:29:21 -0700

    2

--- projects/xujing/part2/tester1.py
+++ projects/xujing/part2/tester1.py
@@ -3,7 +3,27 @@ import random
 import sys
 import time
 
+#begin
+timeout = int(sys.argv[1])
+seed = int(sys.argv[2])
+depth = int(sys.argv[3])
+width = int(sys.argv[4])
+faults = int(sys.argv[5])
+coverage = int(sys.argv[6])
+running = int(sys.argv[7])
 
+actCount = 0
+sut = sut.sut()
+bugs = 0
+coverageCount = {}
+fullPool = []
+error=[]
+noerror=[]
+belowMean = set([])
+start = time.time()
+ntests = 0
+
+print "STARTING PHASE 1"
 def randomAction():
     global actCount, bugs, coverageCount, belowMean,lastAddCoverage
 
@@ -11,108 +31,67 @@ def randomAction():
     actCount += 1
     ok = sut.safely(act)
 
-    if int(sys.argv[7]):
+    if running:
         if len(sut.newBranches()) > 0:
-            print "ACTION:",sut.randomEnabled(random.Random(int(sys.argv[2])))[0]
+            print "ACTION:",sut.randomEnabled(random.Random(seed))[0]
             for b in sut.newBranches():
                 print time.time() - start, len(sut.allBranches()),"New branch",b
+            for s1 in sut.newStatements():
+                print time.time() - start, len(sut.allStatements()),"New statement",b
 
-
-    if not ok:
+    if not ok or not sut.check():
         bugs += 1
         print "FOUND A FAILURE"
-        print sut.failure()
-        print "REDUCING"
-        error.append(sut.test())
-        for s in sut.currStatements():
-            if s not in coverageCount:
-                coverageCount[s] = 0
-            coverageCount[s] += 1
-
-        R = sut.reduce(sut.test(),sut.fails, True, True)
-        sut.prettyPrintTest(R)
-        print sut.failure()
-        fail()
-        sut.restart()
-
-    else:
-        if len(sut.newStatements()) != 0:
-            print "NEW STATEMENTS DISCOVERED",sut.newStatements()
-            oldTest = list(sut.test())
-            storeTest = sut.reduce(oldTest,sut.coversStatements(sut.newStatements()))
-            print "OLD LENGTH = ",len(oldTest),"NEW LENGTH = ",len(storeTest)
-            sut.replay(oldTest)
-            fullPool.append((storeTest, set(sut.currStatements())))
-            lastAddCoverage = set(sut.currStatements())
-            return
-        for s in belowMean:
-            if s in sut.currStatements() and s not in lastAddCoverage:
-                print "NEW PATH TO LOW COVERAGE STATEMENT",s
-                fullPool.append((list(sut.test()), set(sut.currStatements())))
-                lastAddCoverage = set(sut.currStatements())
-                return
+        if faults:
+            print("SHOW FAULT")
+            print sut.failure()
+            error.append(sut.test())
+            R = sut.reduce(sut.test(),sut.fails, True, True)
+            sut.prettyPrintTest(R)
+            print sut.failure()
+            sut.restart()
 
     return ok
 
-
-
-#begin
-
-depth = int(sys.argv[3])
-width = int(sys.argv[4])
-explore = 0.7
-actCount = 0
-sut = sut.sut()
-bugs = 0
-coverageCount = {}
-activePool = []
-fullPool = []
-error=[]
-noerror=[]
-
-belowMean = set([])
-
-print "SHOW PHASE 1"
-
-start = time.time()
-ntests = 0
-
-while time.time()-start < int(sys.argv[1]):
+while time.time()-start < timeout:
     sut.restart()
     ntests += 1
-    for w in xrange (0,width):
-        for s in xrange(0,depth):
-            if not randomAction():
-                break
-        for s in sut.currStatements():
-            if s not in coverageCount:
-                coverageCount[s] = 0
-            if not (sut.newStatements() in error):
-                noerror.append(sut.currStatements())
-            coverageCount[s] += 1
+    for s in xrange(0,depth):
+        if not randomAction():
+           break
+    for s in sut.currStatements():
+        if s not in coverageCount:
+            coverageCount[s] = 0
+        if not (sut.newStatements() in error):
+            noerror.append(sut.currStatements())
+        coverageCount[s] += 1
 
+print "STARTING PHASE 2"
+start = time.time()
 
+def belowMeans():
+    sortedCov = sorted(coverageCount.keys(), key=lambda x: coverageCount[x])
+    coverSum = sum(coverageCount.values())
+    coverMean = coverSum / (1.0*len(coverageCount))
+    for s in sortedCov:
+        if coverageCount[s] < coverMean:
+            belowMean.add(s)
+        else:
+            break
+while time.time()-start < seed:
+    ntests += 1
+    belowMeans()
+    for s in xrange(0,depth):
+        if not randomAction():
+            break
+    if time.time()-start > timeout:
+        break
 
-def fail():
-    print "SHOW FAILURE "
-    if int(sys.argv[5]):
-        print(sut.failure())
-    error.append(sut.currStatements())
-
-
-
-if int(sys.argv[6]):
+if coverage:
     print "SHOW COVERAGE"
     sut.internalReport()
 
-
-
 print ntests,"TESTS"
-
-
-for (t,s) in fullPool:
-    print len(t),len(s)
-
 
 print bugs,"FAILED"
 print "TOTAL ACTIONS",actCount
