d49111c5d7c894ec3d95eb4304078060d232d33f
Alex Groce <alexgroce@MacBook-Pro.local>
2016-05-09 12:09:51 -0700

    now takes arguments

--- projects/runavl.py
+++ projects/runavl.py
@@ -4,21 +4,23 @@ import sys
 import shutil
 
 seed = sys.argv[1]
+time = sys.argv[2]
 
 testers = glob.glob("*/tester1.py")
 testers.extend (glob.glob("*/*/tester1.py"))
 
+timeout = str(int(time)+5)
 
 for t in testers:
     student = t.split("/")[0]
     print student,t
     shutil.copyfile(t,"tester1.py")
-    r = subprocess.call(["ulimit -t 45; python tester1.py 40 " + seed + " 100 10 0 1 1 >& " + student + ".avl.40s." + seed + ".tout"],shell=True)
+    r = subprocess.call(["ulimit -t " + timeout "; python tester1.py " + time + " " + seed + " 100 10 0 1 1 >& " + student + ".avl." + time + "s." + seed + ".tout"],shell=True)
     if r == 152:
         print "TIMEOUT"
-        subprocess.call(["echo !TIMEOUT! >> " + student + ".avl.40s." +seed + ".tout"], shell=True)
+        subprocess.call(["echo !TIMEOUT! >> " + student + ".avl." + time + "s." +seed + ".tout"], shell=True)
     elif r != 0: 
         print "CRASHED"
-        subprocess.call(["echo !CRASHED! >> " + student + ".avl.40s." +seed + ".tout"], shell=True)        
+        subprocess.call(["echo !CRASHED! >> " + student + ".avl." + time + "s." +seed + ".tout"], shell=True)        
 
         
