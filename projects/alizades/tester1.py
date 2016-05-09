7d7da57ff89e31b059d1525b3647389a9860c792
Sahar Alizadeh <cs362sp16@gmail.com>
2016-05-09 14:53:02 -0700

    Update tester1.py

--- projects/alizades/tester1.py
+++ projects/alizades/tester1.py
@@ -9,7 +9,7 @@ import sut
 # parsing parameters
 
 
-rgen = random.Random()
+
 TIME_OUT = int(sys.argv[1])
 
 SEED = int(sys.argv[2])
@@ -27,7 +27,7 @@ RUNNING= int(sys.argv[7])
 sut = sut.sut();
 
 sut.silenceCoverage()
-rand = random.Random()
+
 
 savedTest = None
 
@@ -36,30 +36,47 @@ actCount = 0
 bugs = 0
 i = 0 
 
-act = sut.randomEnabled(rgen)
+rgen = random.Random(SEED)
 
 start = time.time()
 
 #tryStutter = (act != None) and (act[1]())
 storedTest = False;
 while(time.time()< start + TIME_OUT):
-        for ts in xrange(0,WIDTH):
+    # for ts in xrange(0,WIDTH):
             sut.restart()
             for b in xrange(0,DEPTH):
+                act = sut.randomEnabled(rgen)
                 ok = sut.safely(act)
-                new = False
-                actCount += 1
-                propok = sut.check()
-               
-                #if running=1, print elapsed time, total brach count, new branch otherwise don't print
+                if not ok:
+                  bugs+=1
+                
                 if RUNNING:
+                    #print " in running"
                     if sut.newBranches() != set([]):
-#                        print "ACTION:",a[0],tryStutter
+                        #                        print "ACTION:",a[0],tryStutter
                         for b in sut.newBranches():
                             print time.time()-start,len(sut.allBranches())," branch",b
                         new = True
                     else:
-                        new = False  
+                        new = False
+            
+
+                
+                
+                
+                #new = False
+                actCount += 1
+                propok = sut.check()
+
+                
+                
+                
+                
+                
+                #if running=1, print elapsed time, total brach count, new branch otherwise don't print
+
+
 if (COVERAGE):
     sut.internalReport()
-    print "TOTAL NUMBER OF BUGS",bugs
+print "TOTAL NUMBER OF BUGS",bugs
