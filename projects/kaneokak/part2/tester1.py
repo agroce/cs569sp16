f7554580a5977b2b12bbdcb15b149b56b61ac735
kazuki71 <kaneokaka@gmail.com>
2016-05-09 10:29:55 -0700

    change variable name

--- projects/kaneokak/part2/tester1.py
+++ projects/kaneokak/part2/tester1.py
@@ -5,7 +5,53 @@ import random
 import sut
 import sys
 import time
-from collections import defaultdict
+
+def argFaults(fid):
+	filename = 'failure' + `fid` + '.test'
+	f = open(filename, 'w')
+	f.write(str(sut.failure()))
+	f.close
+
+def argRunning(a):
+	if sut.newBranches() != set([]):
+		print "ACTION:", a[0]
+		for b in sut.newBranches():
+			print time.time() - start, len(sut.allBranches()), "New branch", b
+	
+def contracts(seq, errorseqs, ok, propok):
+	if (not ok) or (not propok):
+		print 'FIND BUGS!!'
+		if faults:
+			fid += 1
+			argFaults(fid)
+			errorseqs.append(seq)
+		return True
+	return False
+
+def filters(seq, classTable, ok, propok):
+	notmany = max(classTable.values()) <= width
+	lessdepth = len(seq) <= depth
+	return (ok and propok and lessdepth and notmany)
+
+def oldseq(seq, seqs):
+	setSeq = set(seq)
+	for s in seqs:
+		if setSeq <= set(s):
+			return True
+	return False
+
+def printSeq(seq):
+	print "printing seq..."
+	for a in seq:
+		print a[0]
+	print ""
+
+def printSeqs(seqs):
+	print "printing seqs..."
+	for seq in seqs:
+		for a in seq:
+			print a[0]
+		print ""
 
 timeout = int(sys.argv[1])
 seed = int(sys.argv[2])
@@ -15,137 +61,52 @@ faults = int(sys.argv[5])
 coverage = int(sys.argv[6])
 running  = int(sys.argv[7])
 
-#timeout = 30
-#seed = 1
-#depth = 100
-#width = 1
-#faults = 1
-#coverage = 1
-#running = 1
-
-def actionClassPools(sut):
-	dic = defaultdict(list)
-	for a in sut.actions():
-		dic[sut.actionClass(a)].append(a)
-	return dic
-
-def argFaults(failureCount, sut):
-	filename = 'failure' + `failureCount` + '.test'
-	f = open(filename, 'w')
-	f.write(str(sut.failure()))
-	f.close
-
-def argRunning(s, sut, start):
-	if sut.newBranches() != set([]):
-		print "ACTION:", s[0]
-		for b in sut.newBranches():
-			print time.time() - start, len(sut.allBranches()), "New branch", b
-
-def initComponents(acPools, sut):
-	components = []
-	for k, v in acPools.iteritems():
-		if not sut.dependencies(k):
-			components.extend(v)
-	return components
-
-def isNotContracts(newSeq, ok, propok, sut, faults, failureCount, errorSeqs):
-	if (not ok) or (not propok):
-		print 'FIND BUGS!!'
-		if faults:
-			failureCount += 1
-			argFaults(failureCount, sut)
-			errorSeqs.append(newSeq)
-		return True
-	return False
-
-def isEnabledValue(sut):
-	flag = False
-	for v in sut.state()[3].itervalues():
-		flag or v
-	return flag
-
-def isFilters(newSeq, classTable, depth, width, ok, propok):
-	notmany = max(classTable.values()) <= width
-	lessdepth = len(newSeq) <= depth
-	return (ok and propok and lessdepth and notmany)
-
-def isNotNewSeq(seq, seqs):
-	setSeq = set(seq)
-	for s in seqs:
-		if setSeq <= set(s):
-			return True
-	return False
-
-def isSeqsEqual(seqs):
-	flag = False
-	pre = set()
-	for l in seqs:
-		cur = set(l)
-		flag = cur == pre
-		pre = cur
-	return flag
-
-def printSeq(seq):
-	print "printing seq..."
-	for s in seq:
-		print s[0]
-	print ""
-
-def printSeqs(seqs):
-	print "printing seqs..."
-	for l in seqs:
-		for s in l:
-			print s[0]
-		print ""
-
 sut = sut.sut()
 rgen = random.Random()
 rgen.seed(seed)
-failureCount = 0
-errorSeqs = []
-nonErrorSeqs = [[]]
+fid = 0
+errorseqs = []
+nonerrorseqs = [[]]
+
 start = time.time()
 while time.time() - start < timeout:
 	sut.restart()
-	newSeq = rgen.choice(nonErrorSeqs)[:]
+	seq = rgen.choice(nonerrorseqs)[:]
 	classTable = dict.fromkeys(sut.actionClasses(), 0)
-	for s in newSeq:
-		s[2]()
-		classTable[sut.actionClass(s)] += 1
+	for a in seq:
+		a[2]()
+		classTable[sut.actionClass(a)] += 1
 
 	if rgen.randint(0, 9) == 0:
 		n = rgen.randint(2, 100)
 		while n > 0:
 			n -= 1
 			a = sut.randomEnabled(rgen)	
-			newSeq.append(a)
-			if isNotNewSeq(newSeq, errorSeqs) or isNotNewSeq(newSeq, nonErrorSeqs):
+			seq.append(a)
+			if oldseq(seq, errorseqs) or oldseq(seq, nonerrorseqs):
 				continue
 			ok = sut.safely(a)
 			propok = sut.check()
 			if running:
-				argRunning(a, sut, start)
-			if isNotContracts(newSeq, ok, propok, sut, faults, failureCount, errorSeqs):
+				argRunning(a)
+			if contracts(seq, errorseqs, ok, propok):
 				break
 			classTable[sut.actionClass(a)] += 1
 	else:
 		a = sut.randomEnabled(rgen)	
-		newSeq.append(a)
-		if isNotNewSeq(newSeq, errorSeqs) or isNotNewSeq(newSeq, nonErrorSeqs):
+		seq.append(a)
+		if oldseq(seq, errorseqs) or oldseq(seq, nonerrorseqs):
 			continue
 		ok = sut.safely(a)
 		propok = sut.check()
 		if running:
-			argRunning(a, sut, start)
-		if isNotContracts(newSeq, ok, propok, sut, faults, failureCount, errorSeqs):
+			argRunning(a)
+		if contracts(seq, errorseqs, ok, propok):
 			break
 		classTable[sut.actionClass(a)] += 1
 
-	if isFilters(newSeq, classTable, depth, width, ok, propok):
-		nonErrorSeqs.append(newSeq)
-
-	#printSeq(newSeq)
-	#printSeqs(nonErrorSeqs)
+	if filters(seq, classTable, ok, propok):
+		nonerrorseqs.append(seq)
 
 if coverage:
 	sut.internalReport()
