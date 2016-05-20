3d396e35a56dca931d0e3bb834179a3497b30568
qrslrhko <qrslrhko@gmail.com>
2016-05-20 00:44:48 -0700

    fixed

--- projects/yangyic/tester2.py
+++ projects/yangyic/tester2.py
@@ -12,21 +12,22 @@ FAULTS = int(sys.argv[5])
 COVERAGE = int(sys.argv[6])
 RUNNING = int(sys.argv[7])
 
-def Find_bugs(a):
-	#print "aaaaa"
+def Find_bugs(list_actions):
+	# print "Find_bugs"
 	global coverage_counter
 	global bugs
+	bugs = 0	
 	global Record
 	global string
-	safe = sut.safely(a)
+	safe = sut.safely(list_actions)
 	running_oh()
 	if not safe:
 		print " *** find a bug: "
 		bugs = bugs + 1
-		for s in sut.currStatements():
-			if s not in coverage_counter:
-				coverage_counter[s]=0
-			coverage_counter[s] = coverage_counter[s] + 1	
+		for t in sut.currStatements():
+			if t not in coverage_counter:
+				coverage_counter[t]=0
+			coverage_counter[t] = coverage_counter[t] + 1	
 		# Collect_coverage()
 		print sut.failure()
 		# string = str(sut.failure)
@@ -35,7 +36,7 @@ def Find_bugs(a):
 		# R = sut.reduce(sut.test(),sut.fails, True, True)
 		# sut.prettyPrintTest(R)	
 		sut.restart()
-
+	return safe
 
 def divide_coverage():
 	global four_coverage,three_coverage, two_coverage, four_coverage
@@ -95,6 +96,8 @@ def record_failures():
 # <timeout> <seed> <depth> <width> <faults> <coverage> <running>
 
 sut = sut.sut()
+
+sut.silenceCoverage()
 nums = random.Random() 
 bugs = 0
 coverage_counter = {}
@@ -128,33 +131,37 @@ median_odd = set([])
 
 
 while time.time() - start_time_1 < (TIMEOUT*0.87):
-	# for dep in xrange(1,DEPTH):
+	sut.restart()
+	for dep in xrange(1,DEPTH):
 		dep = dep +1 
 		queue_list =[]
 		inside_time = time.time()
-		sut.restart()
+		# sut.restart()
 		for x in queue_q:
 			ran_queue.append(x)
 			sut.backtrack(x)
 			elapsed = time.time() - start_time_1
-			list_actions = sut.enabled()
+			list_actions = sut.randomEnabled(regn)
+			# print "list_actions>>>>",list_actions
+			# list_actions = sut.enabled()
 			# actinos = list_actions
-			regn.shuffle(list_actions)	
-			for a in list_actions:
-				divide_coverage()
-				if time.time() - inside_time >= 3:
-					break
-				Find_bugs(a)	
-				s2 = sut.state()
-				if s2 not in ran_queue:	
-					ran_queue.append(s2)
-					queue_list.append(s2)	
-				sut.backtrack(x)	
+			# regn.shuffle(list_actions)	
+			# Find_bugs(list_actions)
+			# for a in list_actions:
+			divide_coverage()
+			if time.time() - inside_time >= 3:
+				break
+			Find_bugs(list_actions)	
+			s2 = sut.state()
+			if s2 not in ran_queue:	
+				ran_queue.append(s2)
+				queue_list.append(s2)	
+			sut.backtrack(x)	
 			# Collect_coverage()	
-			for s in sut.currStatements():
-				if s not in coverage_counter:
-					coverage_counter[s]=0
-				coverage_counter[s] = coverage_counter[s] + 1	
+		for s in sut.currStatements():
+			if s not in coverage_counter:
+				coverage_counter[s]=0
+			coverage_counter[s] = coverage_counter[s] + 1	
 		queue_q = queue_list
 		
 
