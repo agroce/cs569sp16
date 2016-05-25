71ecec0e04a4f4e26bc16d80eb6033131ebfe1e2
hsnla <hassanlashari@Hassans-MacBook-Pro-2.local>
2016-05-25 00:00:54 -0700

    uploading for bangashm

--- projects/bangashm/tester2.py
+++ projects/bangashm/tester2.py
@@ -21,7 +21,7 @@ def check_action():
     elapsed = time.time() - start
     if config.running:
         if len(sut.newBranches()) > 0:
-            print "Action count:", action[0]
+            print "Action count:", action[0]                              #this will count the no of actions
             for b in sut.newBranches():
                 print elapsed, len(sut.allBranches()),"New branch",b
     
@@ -30,31 +30,30 @@ def check_action():
         print "Bug Found" , num
         print "Shrinking now"
         startReduce = time.time()
-        test = sut.reduce(sut.test(), sut.fails, True, True)
-        sut.prettyPrintTest(test)
+        Z = sut.reduce(sut.test(), sut.fails, True, True)
+        sut.prettyPrintTest(Z)
         print(sut.failure())
         if config.fault:
-            f = open((file_name + str(num) + ".test"),"w")
-            f.writelines(str(sut.failure())) 
-            f.writelines('\nReduced test now has ' + str(len(test)) + ' steps')
+            Z = 'failure'+str(num)+'.test'
+	    sut.saveTest(Z,FileName)
             f.close()
    
     return ok
  
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('timeout', type=int, default=60)
-    parser.add_argument('seed', type=int, default=None)
-    parser.add_argument('depth', type=int, default=100)
-    parser.add_argument('width', type=int, default=100)
-    parser.add_argument('fault', type=int, default=0)
-    parser.add_argument('coverage', type=int, default=0)
-    parser.add_argument('running', type=int, default=0)
+    parser.add_argument('timeout', type=int, default=60)            #this will take timeout
+    parser.add_argument('seed', type=int, default=None)             #this will take seed
+    parser.add_argument('depth', type=int, default=100)             #this will take depth
+    parser.add_argument('width', type=int, default=100)             #this will take width
+    parser.add_argument('fault', type=int, default=0)               #this will take fault
+    parser.add_argument('coverage', type=int, default=0)            #this will take coverage
+    parser.add_argument('running', type=int, default=0)             #this will take running parameteres
     parsed_args = parser.parse_args(sys.argv[1:])
     return (parsed_args, parser)
 
 def main():
-    global start,config,sut,R,nonerror,error,file_name,num,actioncount
+    global start,config,sut,R,nonerror,error,file_name,num,actioncount,Z
     num = 0
     actioncount = 0
     file_name = 'failurefile'
@@ -76,9 +75,9 @@ def main():
 	    i += 1
 	    if (time.time() > start + config.timeout):
                 break
-            sut.restart()
+            sut.restart()                                                #the sut is restarted 
 
-            sut.backtrack(s)
+            sut.backtrack(s)                                             
 
             for w in xrange(0, config.width):
 
@@ -101,19 +100,18 @@ def main():
 
                         nonerror.append(sut.currStatements())
 
-		if (config.fault):
-			    f = open("failure_file" + str(failureCount) + ".test", "w")
-			    j = 0
-		       	    for (s_reduces, _, _) in S:
-				    steps_reduce = "# STEP " + str(j)
-				    print >> f, sut.prettyName(s_reduces).ljust(80 - len(steps_reduce), ' '), steps_reduce
-				    j += 1
-			    f.close()
+		#if (config.fault):                                                      #if fault is found
+	                    
+		       	#    for (s_reduces, _, _) in Z:
+			#	    steps_reduce = "# STEP " + str(j)
+			#	    print >> f, sut.prettyName(s_reduces).ljust(80 - len(steps_reduce), ' '), steps_reduce
+			#	    j += 1
+			  #  f.close()
 
                              
     if config.coverage:
 
-        sut.internalReport()
+        sut.internalReport()                                                           #this will print the internal report
 
     
     print "Running time: ", time.time() - start
@@ -121,7 +119,6 @@ def main():
     print "No. of actions: ", actioncount
     
     
-
 
 
 if __name__ == '__main__':
