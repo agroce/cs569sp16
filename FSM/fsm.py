752c23e965be5f42e5b9c0ed89ce608a9a009e5d
Alex Groce <alexgroce@MacBook-Pro.local>
2016-05-25 14:14:23 -0700

    changes

--- FSM/fsm.py
+++ FSM/fsm.py
@@ -40,11 +40,21 @@ class Machine:
                 self.state = 4
                 return 1
             if alpha == 'b':
-                self.state = 4
+                self.state = 5
                 return 1                        
             if alpha == 'c':
                 self.state = 0
                 return 1
+        if self.state == 5:
+            if alpha == 'a':
+                self.state = 0
+                return 1
+            if alpha == 'b':
+                self.state = 5
+                return 1                        
+            if alpha == 'c':
+                self.state = 5
+                return 1            
         return 0
 
     def specinputs(self,str):
@@ -54,3 +64,9 @@ class Machine:
             res.append((c,self.input(c)))
         self.state = backup
         return res
+
+    def inputs(self,str):
+        res = []
+        for c in str:
+            res.append((c,self.input(c)))
+        return res    
