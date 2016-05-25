77035a72fe18ea3bf617dfbbb2915cc99ef19ddf
tangzhen-osu <tangz@oregonstate.edu>
2016-05-24 23:48:21 -0700

    update

--- projects/tangz/tester2.py
+++ projects/tangz/tester2.py
@@ -93,7 +93,7 @@ def main():
                      print "STOPPING TESTING DUE TO FAILED TEST"
                      failCount+=1
                      if config.faults == 1:
-                             f = open("failure" + str(failCount) + ".test", "w")
+                             f = "failure" + str(failCount) + ".test", "w"
                              t.saveTest(t.test(),f) 
                      break      
               
