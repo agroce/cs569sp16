1e5ba60b63f8f36daa7250c05e27cd325629750b
JingyuanXu <skybluries@gmail.com>
2016-05-31 23:06:55 -0700

    update 3

--- projects/xujing/tester2.py
+++ projects/xujing/tester2.py
@@ -13,7 +13,7 @@ running = int(sys.argv[7])
 
 
 def randomAction():
-    global actCount, bugs,newseq
+    global actCount, bugs,newseq,currseq
     act = sut.randomEnabled(R)
     actCount += 1
     ok = sut.safely(act)
@@ -29,47 +29,63 @@ def randomAction():
     if not ok:
         bugs += 1
         print "FOUND A FAILURE"
-        error.append(newseq)
+
         if faults:
-            print("SHOW FAULT")
             f = sut.reduce(sut.test(), sut.fails, True, True)
             sut.prettyPrintTest(f)
-            f = 'failure' + str(actCount) + '.test'
-            sut.saveTest(sut.test(), f)
+            currseq.append((f,set(sut.currStatements())))
+            print("SHOW FAULT")
+            file = 'failure' + str(actCount) + '.test'
+            sut.saveTest(sut.test(), file)
             sut.restart()
             print sut.failure()
 
     return ok
 
+def flags(newseq,c):
+    pass
+
+
 def checkAlg():
-    global error, noerror
-    if not error ==[]:
-        print "SHOW ERROR SEQUENCE"
+    global error, noerror,newseq,currseq,states
+     #newseq = sut.randomEnableds(R, depth)
+    newseq=sut.newStatements()
+    c=sut.check()
+    if c:
+        if(not ((newseq in error) or (newseq in noerror))):
+            states.insert(ntest-1,sut.state())
+            noerror.append(sut.currStatements())
+            flags(newseq,c)
+    else:
+        error.append(sut.currStatements())
+
+    #if not error ==[]:
+    #    print "SHOW ERROR SEQUENCE"
         #    print error
         #f = open(("failure" + str(actCount) + ".test"), 'w')
         #f.write(str(error))
         #f.close()
-    else:
-        print "Data in noerror sequence"
+    #else:
+    #    print "Data in noerror sequence"
         # print noerror
 
 def main():
-    global start,sut,R,noerror,error,actCount, bugs
+    global start,sut,R,noerror,error,actCount, bugs,ntest,newseq,currseq,states
     actCount = 0
     bugs = 0
     start = time.time()
     noerror = []
     error = []
     newseq = []
-    ntest = 0
-    coverageCount = {}
+    ntest=0
+    currseq=[]
     sut = sut.sut()
     R = random.Random(seed)
     states = [sut.state()]
     print "STARTING PHASE 1"
     while(time.time() < start + timeout):
         for st in states:
-            ntest += 1
+            ntest+=1
             if (time.time() > start + timeout):
                 break
             sut.restart()
@@ -77,14 +93,10 @@ def main():
             for s in xrange(0, depth):
                 if (time.time() > start + timeout):
                     break
-                newseq = sut.randomEnableds(R, depth)
                 ok = randomAction()
                 if not ok:
                     break
-                if(not ((newseq in error) or (newseq in noerror))):
-                    noerror.append(newseq)
-
-    checkAlg()
+                checkAlg()
 
     if coverage:
         sut.internalReport()
