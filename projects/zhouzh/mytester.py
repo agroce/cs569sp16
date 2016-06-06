4ff7afd0b7c3806a09cf22574872424242ed4f03
Encore Chow <encorechow1992@gmail.com>
2016-06-05 23:46:43 -0700

    fixed bugs in mytester

--- projects/zhouzh/mytester.py
+++ projects/zhouzh/mytester.py
@@ -20,7 +20,7 @@ def parse_args():
      parser.add_argument('-t', '--timeout', type=int, default=60, help='Timeout in seconds. (60 default)')
      parser.add_argument('-s', '--seed', type=int, default=None, help='Random seed. (default = None)')
      parser.add_argument('-d', '--depth', type=int, default=100, help='Maximum search depth. (100 default)')
-     parser.add_argument('-w', '--width', type=int, default=10000, help='Maximum memory. (10000 default)')
+     parser.add_argument('-w', '--width', type=int, default=10, help='Maximum memory. (10000 default)')
      parser.add_argument('-f', '--faults', type=int, default=0, choices=[0, 1], help='Check for faults or not. 1 for check, 0 for do not check (0 default)')
      parser.add_argument('-c', '--coverage', type=int, default=0, choices=[0, 1] ,help='report coverage or not. 1 for report, 0 for do not report(0 default)')
      parser.add_argument('-r', '--running', type=int, default=0, choices=[0, 1], help='Produce running branch coverage report.')
@@ -55,7 +55,7 @@ def saveFaults(sut, nbugs, Config, faults_file):
         sut.saveTest(reduction, faults_file + str(nbugs) + ".test")
 
 
-def mutate(test, r, nbugs):
+def mutate(test, r, nbugs, sut, Config):
     tcopy = list(test)
     i = r.randint(0,len(tcopy))
     sut.replay(tcopy[:i])
@@ -65,15 +65,63 @@ def mutate(test, r, nbugs):
         nbugs += 1
         saveFaults(sut)
         sut.restart()
+    if Config.running:
+        if sut.newBranches() != set([]):
+            print "ACTION:", act[0]
+            for b in sut.newBranches():
+                print time.time()-start, len(sut.allBranches()), "New branch", b
+        if sut.newStatements() != set([]):
+            print "ACTION:", act[0]
+            for s in sut.newStatements():
+                print time.time()-start, len(sut.allStatements()),"New statement",s
     else:
         trest = [e]
         for s in tcopy[i+1:]:
             if s[1]():
                 trest.append(s)
-                sut.safely(s)
+                isGood = sut.safely(s)
+                if not isGood:
+                    nbugs += 1
+                    saveFaults(sut)
+                    sut.restart()
+                if Config.running:
+                    if sut.newBranches() != set([]):
+                        print "ACTION:", act[0]
+                        for b in sut.newBranches():
+                            print time.time()-start, len(sut.allBranches()), "New branch", b
+                    if sut.newStatements() != set([]):
+                        print "ACTION:", act[0]
+                        for s in sut.newStatements():
+                            print time.time()-start, len(sut.allStatements()),"New statement",s
                 tcopy = test[:i]+trest
     return tcopy
 
+
+def crossover(test, test2, r, nbugs, sut, Config):
+    tcopy = list(test)
+    i = r.randint(0,len(tcopy))
+    sut.replay(tcopy[:i])
+    trest = []
+    for s in test2[i:]:
+        if s[1]():
+            trest.append(s)
+            isGood = sut.safely(s)
+            if Config.running:
+                if sut.newBranches() != set([]):
+                    print "ACTION:", act[0]
+                    for b in sut.newBranches():
+                        print time.time()-start, len(sut.allBranches()), "New branch", b
+                if sut.newStatements() != set([]):
+                    print "ACTION:", act[0]
+                    for s in sut.newStatements():
+                        print time.time()-start, len(sut.allStatements()),"New statement",s
+            if not isGood:
+                nbugs += 1
+                saveFaults(sut)
+                sut.restart()
+    tcopy = test[:i]+trest
+
+    return tcopy
 
 
 def main():
@@ -130,6 +178,8 @@ def main():
                 print "Population Stopping Due To Timeout, Terminated at Length", len(sut.test())
                 print time.time()-start, "TOTAL RUNTIME"
                 print ntests, "EXECUTED"
+                print "Statements: ", len(sut.allStatements())
+                print "Branches: ", len(sut.allBranches())
                 break
 
     mutate_start = time.time()
@@ -139,19 +189,12 @@ def main():
             (t,s) = R.choice(population)
         else:
             (t,s) = R.choice(sortedPop[:Config.number])
-        m = mutate(t, R, nbugs)
+        (t2,s) = R.choice(sortedPop[:Config.number])
+        c = crossover(t, t2, R, nbugs, sut, Config)
+        m = mutate(t, R, nbugs, sut, Config)
         elapsed = time.time() - mutate_start
         population.append((m, sut.currStatements()))
-
-        if Config.running:
-            if sut.newBranches() != set([]):
-                print "ACTION:", act[0]
-                for b in sut.newBranches():
-                    print time.time()-start, len(sut.allBranches()), "New branch", b
-            if sut.newStatements() != set([]):
-                print "ACTION:", act[0]
-                for s in sut.newStatements():
-                    print time.time()-start, len(sut.allStatements()),"New statement",s
+        population.append((c, sut.currStatements()))
 
         if elapsed >= Config.timeout / 3:
             print "Stopping Test Due To Timeout, Terminated at Length", len(sut.test())
