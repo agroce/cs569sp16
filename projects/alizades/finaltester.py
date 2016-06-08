104a8958a79c22bfa75543123da5246af45b882d
Sahar Alizadeh <cs362sp16@gmail.com>
2016-06-07 21:15:39 -0700

    Update finaltester.py

--- projects/alizades/finaltester.py
+++ projects/alizades/finaltester.py
@@ -84,9 +84,9 @@ while(time.time()< start + TIME_OUT):
                         bugs += 1
                         fault = sut.failure();
                         fname = 'fault'+str(bugs)+'.test'
-                        #wfile = open(fname,'w')
-                        #wfile.write(str(fault))
-                        #wfile.close()
+                        wfile = open(fname,'w')
+                        wfile.write(str(fault))
+                        wfile.close()
                         sut.restart()
                 else:
                     if len(sut.newBranches()) != 0:
