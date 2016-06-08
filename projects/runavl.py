00bbfefe842ed47c37ee08312e7cd6bef68ae59a
Alex Groce <alexgroce@10-248-126-13.wireless.oregonstate.edu>
2016-06-08 11:33:52 -0700

    run avl script

--- projects/runavl.py
+++ projects/runavl.py
@@ -6,16 +6,20 @@ import shutil
 seed = sys.argv[1]
 time = sys.argv[2]
 
-testers = glob.glob("*/tester1.py")
-testers.extend (glob.glob("*/*/tester1.py"))
+testers = glob.glob("*/tester2.py")
 
 timeout = str(int(time)+5)
 
 for t in testers:
     student = t.split("/")[0]
     print student,t
-    shutil.copyfile(t,"tester1.py")
-    r = subprocess.call(["ulimit -t " + timeout + "; python tester1.py " + time + " " + seed + " 100 10 0 1 1 >& " + student + ".avl." + time + "s." + seed + ".tout"],shell=True)
+    shutil.copyfile(t,"tester2.py")
+    for f in glob.glob("*.test"):
+        subprocess.call(["rm -rf " + f],shell=True)
+    subprocess.call(["mkdir tests."+ student],shell=True)
+    r = subprocess.call(["ulimit -t " + timeout + "; python tester2.py " + time + " " + seed + " 100 10 1 1 1 >& " + student + ".avl." + time + "s." + seed + ".tout"],shell=True)
+    for f in glob.glob("*.test"):
+        subprocess.call(["mv " + f + " tests." + student], shell=True)
     if r == 152:
         print "TIMEOUT"
         subprocess.call(["echo !TIMEOUT! >> " + student + ".avl." + time + "s." +seed + ".tout"], shell=True)
